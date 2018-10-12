bba94357a4232f9b002e2bb6b52edf36838e8353
Miles Perkins <mperkins1993@gmail.com>
2018-10-11 22:45:20 -0400

    first commit

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import Moment from "react-moment";
+
+/*
+  Lyrics component will make 2 calls to the musixmatch api to retrieve the 
+  actual lyrics and then the track information since the track info is not attached 
+  to the lyrics object inside the componentDidMount().
+  
+  the track call is placed inside the .then() of the lyrics so that it will run based on the success of the 
+*/
+class Lyrics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      track: {},
+      lyrics: {}
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
+          this.props.match.params.id
+        }&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({ track: res.data.message.body.track });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    // some destructuring so we don't have to use this.state.track or this.state.lyrics
+    // Just like before we should use some logic to prevent an empty object response which we will do with some logic
+    const { track, lyrics } = this.state;
+    console.log(track);
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} By{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Genre</strong>:{" "}
+              {
+                track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name
+              }
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit</strong>: {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>:{" "}
+              <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+export default Lyrics;
