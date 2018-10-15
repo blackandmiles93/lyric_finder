bdee19c7c80bf4b9b1c1233134df8a2e741d307a
Miles Perkins <mperkins1993@gmail.com>
2018-10-15 00:07:13 -0400

    Adding initial okta setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
 import Lyrics from "./components/tracks/Lyrics";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+import config from "./.ok.config.js";
 
 import { Provider } from "./context";
 
@@ -13,15 +15,29 @@ class App extends Component {
     return (
       <Provider>
         <Router>
-          <React.Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Index} />
-                <Route exact path="/lyrics/track/:id" component={Lyrics} />
-              </Switch>
-            </div>
-          </React.Fragment>
+          <Security
+            issuer={config.oidc.issuer}
+            redirect_uri={config.oidc.redirect_uri}
+            client_id={config.oidc.client_id}
+          >
+            <React.Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Index} />
+                  <Route
+                    path="/implicit/callback"
+                    component={ImplicitCallback}
+                  />
+                  <SecureRoute
+                    exact
+                    path="/lyrics/track/:id"
+                    component={Lyrics}
+                  />
+                </Switch>
+              </div>
+            </React.Fragment>
+          </Security>
         </Router>
       </Provider>
     );
--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import Tracks from "../tracks/Tracks";
 import Search from "../tracks/Search";
+import { withAuth } from "@okta/okta-react";
 
-const Index = () => {
-  return (
-    <React.Fragment>
-      <Search />
-      <Tracks />
-    </React.Fragment>
-  );
-};
-export default Index;
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { authenticated: null };
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+    this.checkAuthentication();
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  async checkAuthentication() {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated: authenticated });
+    }
+  }
+
+  componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  async login() {
+    this.props.auth.login("/");
+  }
+
+  async logout() {
+    this.props.auth.logout("/");
+  }
+
+  render() {
+    console.log(this.props.auth);
+    if (this.state.authenticated === null) {
+      return <h1>Please Login</h1>;
+    } else {
+      return (
+        <div>
+          {this.state.authenticated ? (
+            <React.Fragment>
+              <button onClick={this.logout}>Logout</button>
+              <Search />
+              <Tracks />
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <button onClick={this.login}>Login</button>
+            </React.Fragment>
+          )}
+        </div>
+      );
+    }
+
+    // return (
+    //   <React.Fragment>
+    //     <div className="login">
+    //       <button onClick={this.login}>Login</button>
+    //       <button onClick={this.logout}>Logout</button>
+    //     </div>
+    //     <Search />
+    //     <Tracks />
+    //   </React.Fragment>
+    // );
+  }
+}
+export default withAuth(Index);
